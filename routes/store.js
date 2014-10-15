ca9d794cf65169a9a335d6841bf1c86b038a0b79
Sagar113 <SAGAR@sagaras-MacBook-Pro.local>
2014-10-15 15:53:47 +0545

    line 12

--- routes/store.js
+++ routes/store.js
@@ -9,19 +9,19 @@ var items = {
 };
 exports.home = function(req, res) {
     // if user is not logged in, ask them to login
-    // if (typeof req.session.username == 'undefined') 
-    	res.render('home', { title: 'Ninja Store'});
+    if (typeof req.session.username == 'undefined') 
+    	{res.render('home', { title: 'Ninja Store'});}
     // if user is logged in already, take them straight to the items list
-    // else res.redirect('/items');
+    else res.redirect('/items');
 };
 // handler for form submitted from homepage
 exports.home_post_handler = function(req, res) {
     // if the username is not submitted, give it a default of "Anonymous"
     username = req.body.username || 'Anonymous';
     // store the username as a session variable
-    // req.session.username = username;
+    req.session.username = username;
     // redirect the user to homepage
-    res.redirect('/items');
+    res.redirect('/');
 };
 /* GET home page. */
 // handler for displaying the items
